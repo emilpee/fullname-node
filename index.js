7015faccc7d1feda56d3374ee106fecfb35e466a
Emil Petersson <emilpet93@gmail.com>
2019-02-01 13:26:57 +0100

    Removed capital letters and console logs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Läs in filesystem-modul
-const Fs = require('fs');
+const fs = require('fs');
 
 // Paths
 var firstName = './firstName.txt';
@@ -12,15 +12,13 @@ var options = {
     flag: 'r'
 }
 
-// Läs in filer
-Fs.readFile(firstName, options, (err, firstData) => {
+// Läser in filer
+fs.readFile(firstName, options, (err, firstData) => {
     if (err) throw err;
-    console.log(firstData);
-      Fs.readFile(lastName, options, (err, secondData) => {
+      fs.readFile(lastName, options, (err, secondData) => {
           if (err) throw err;
-          console.log(secondData);
-        var fullData = firstData.concat(' ' + secondData);  // Slår ihop data från båda filer
-        Fs.writeFile(fullName, fullData, (err) => {
+          var fullData = firstData.concat(' ' + secondData);  // Slår ihop data från båda filer
+        fs.writeFile(fullName, fullData, (err) => {
             if (err) throw err;
             console.log('Klart!');
         })
