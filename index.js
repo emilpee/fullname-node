82230e9d48bd657a3e3510a9c48ac54eb50b66e7
Emil Petersson <emilpet93@gmail.com>
2019-02-01 12:16:11 +0100

    Read data from two files and merge them

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const fs = require('fs');
-const http = require('http');
+// Läs in filesystem-modul
+const Fs = require('fs');
 
+// Paths
 var firstName = './firstName.txt';
 var lastName = './lastName.txt';
+var fullName = './fullName.txt'
+
+// Options
+var options = {
+    encoding: 'utf8',
+    flag: 'r'
+}
+
+// Läs in filer
+Fs.readFile(firstName, options, (err, firstData) => {
+    if (err) throw err;
+    console.log(firstData);
+      Fs.readFile(lastName, options, (err, secondData) => {
+          if (err) throw err;
+          console.log(secondData);
+        var fullData = firstData.concat(' ' + secondData);  // Slår ihop data från båda filer
+        Fs.writeFile(fullName, fullData, (err) => {
+            if (err) throw err;
+            console.log('Klart!');
+        })
+    })  
+})
